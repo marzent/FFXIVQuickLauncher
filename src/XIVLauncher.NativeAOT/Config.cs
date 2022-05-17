4c5245b980b299441b53bd7b90a75800cc7ddc71
marzent <marc_aurel@me.com>
2022-05-17 11:32:33 +0200

    create nativeAOT project

---
+++ src/XIVLauncher.NativeAOT/Config.cs
@@ -0,0 +1,50 @@
+﻿using XIVLauncher.Common;
+using XIVLauncher.Common.Addon;
+using XIVLauncher.Common.Dalamud;
+using XIVLauncher.Common.Game.Patch.Acquisition;
+using XIVLauncher.Common.Unix.Compatibility;
+
+namespace XIVLauncher.NativeAOT.Configuration;
+
+public class LauncherConfig
+{
+    public string? AcceptLanguage { get; set; }
+
+    public DirectoryInfo? GamePath { get; set; }
+
+    public DirectoryInfo? GameConfigPath { get; set; }
+
+    public string? AdditionalArgs { get; set; }
+
+    public ClientLanguage? ClientLanguage { get; set; }
+
+    public bool? IsUidCacheEnabled { get; set; }
+
+    public bool? IsDx11 { get; set; }
+
+    public bool? IsEncryptArgs { get; set; }
+
+    public bool? IsFt { get; set; }
+
+    #region Patching
+
+    public DirectoryInfo? PatchPath { get; set; }
+
+    public bool? KeepPatches { get; set; }
+
+    public AcquisitionMethod? PatchAcquisitionMethod { get; set; }
+
+    public long PatchSpeedLimit { get; set; }
+
+    #endregion
+
+    #region Dalamud
+
+    public bool? DalamudEnabled { get; set; }
+
+    public DalamudLoadMethod? DalamudLoadMethod { get; set; }
+
+    public int DalamudLoadDelay { get; set; }
+
+    #endregion
+}
---
+++ src/XIVLauncher.NativeAOT/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Runtime.InteropServices;
+using Serilog;
+using XIVLauncher.Common;
+using XIVLauncher.Common.Dalamud;
+using XIVLauncher.Common.Game.Patch.Acquisition;
+using XIVLauncher.Common.PlatformAbstractions;
+using XIVLauncher.Common.Windows;
+using XIVLauncher.Common.Unix;
+using XIVLauncher.Common.Unix.Compatibility;
+using XIVLauncher.NativeAOT.Configuration;
+
+namespace NativeLibrary;
+
+public class Program
+{
+    public static LauncherConfig? Config { get; private set; }
+
+    [UnmanagedCallersOnly(EntryPoint = "generateAcceptLanguage")]
+    public static IntPtr GenerateAcceptLanguage()
+    {
+        // Needs to be freed by the caller
+        return (IntPtr)Marshal.StringToHGlobalAnsi(Util.GenerateAcceptLanguage());
+    }
+
+    [UnmanagedCallersOnly(EntryPoint = "loadConfig")]
+    public static void LoadConfig(IntPtr acceptLanguage, IntPtr gamePath, IntPtr gameConfigPath, byte clientLanguage, bool isDx11, bool isEncryptArgs, bool isFt, IntPtr patchPath, byte patchAcquisitionMethod, Int64 patchSpeedLimit, bool dalamudEnabled, byte dalamudLoadMethod)
+    {
+        Config = new LauncherConfig
+        {
+            AcceptLanguage = Marshal.PtrToStringAnsi(acceptLanguage),
+
+            GamePath = new DirectoryInfo(Marshal.PtrToStringAnsi(gamePath)!),
+            GameConfigPath = new DirectoryInfo(Marshal.PtrToStringAnsi(gameConfigPath)!),
+            ClientLanguage = (ClientLanguage)clientLanguage,
+
+            IsDx11 = isDx11,
+            IsEncryptArgs = isEncryptArgs,
+            IsFt = isFt,
+
+            PatchPath = new DirectoryInfo(Marshal.PtrToStringAnsi(patchPath)!),
+            PatchAcquisitionMethod = (AcquisitionMethod)patchAcquisitionMethod,
+            PatchSpeedLimit = patchSpeedLimit,
+
+            DalamudEnabled = dalamudEnabled,
+            DalamudLoadMethod = (DalamudLoadMethod)dalamudLoadMethod
+        };
+    }
+}
+
