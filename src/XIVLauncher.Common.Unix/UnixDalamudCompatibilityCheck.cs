b9e1b17fa619e6d81fc73a7c608771e16c231605
Marc-Aurel Zent <marc_aurel@me.com>
2023-02-10 22:28:30 +0100

    remove architecture check from unix dalamud

--- src/XIVLauncher.Common.Unix/UnixDalamudCompatibilityCheck.cs
+++ src/XIVLauncher.Common.Unix/UnixDalamudCompatibilityCheck.cs
@@ -1,4 +1,3 @@
-using System.Runtime.InteropServices;
 using XIVLauncher.Common.PlatformAbstractions;
 
 namespace XIVLauncher.Common.Unix;
@@ -7,27 +6,6 @@ public class UnixDalamudCompatibilityCheck : IDalamudCompatibilityCheck
 {
     public void EnsureCompatibility()
     {
-        //Dalamud will work with wines built-in vcrun, so no need to check that
-        EnsureArchitecture();
-    }
-
-    private static void EnsureArchitecture()
-    {
-        var arch = RuntimeInformation.ProcessArchitecture;
-
-        switch (arch)
-        {
-            case Architecture.X86:
-                throw new IDalamudCompatibilityCheck.ArchitectureNotSupportedException("Dalamud is not supported on x86 architecture.");
-
-            case Architecture.X64:
-                break;
-
-            case Architecture.Arm:
-                throw new IDalamudCompatibilityCheck.ArchitectureNotSupportedException("Dalamud is not supported on ARM32.");
-
-            case Architecture.Arm64:
-                throw new IDalamudCompatibilityCheck.ArchitectureNotSupportedException("x64 emulation was not detected. Please make sure to run XIVLauncher with x64 emulation.");
-        }
+        //Dalamud will work with wines built-in vcrun, so no need to check that or the architecture
     }
 }
